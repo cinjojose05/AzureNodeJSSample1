6d987691a0cba1be284eda6872a5339a156f9dee
Cinjo Jose <cinjojose05@gmail.com>
2026-02-23 23:07:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.json());
 const customersRouter = require('./routes/customers');
 app.use('/api/customers', customersRouter);
 
-app.listen(3000, () => console.log('Server is Started on port 3000'));
+//app.listen(3000, () => console.log('Server is Started on port 3000'));
