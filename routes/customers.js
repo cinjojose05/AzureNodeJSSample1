087786d3b3484ed40707eb0a5c7b06388298e2f0
Cinjo Jose <cinjojose05@gmail.com>
2026-02-27 18:36:10 +0300

    josn resposne

--- routes/customers.js
+++ routes/customers.js
@@ -6,8 +6,8 @@ const customerdb = require('../customerdb'); // Import the customerdb module
 router.get('/', async (req, res) => {
     try {
         const result = await customerdb.connectandQuery(); // Call the function to connect and query the database
-        //res.json(result); // Send the result as JSON response
-        res.send(`All customers: ${JSON.stringify(result)}`); // Send a response indicating that all customers have been retrieved
+        res.json(result); // Send the result as JSON response
+        //res.send(`All customers: ${JSON.stringify(result)}`); // Send a response indicating that all customers have been retrieved
     } catch (error) {
         console.error('Error connecting to the database:', error);
         res.status(500).json({ error: error.message });
