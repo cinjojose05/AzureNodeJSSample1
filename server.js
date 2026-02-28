ea6b8bf6c21227f68f66229d23ef4062235f4da4
Cinjo Jose <cinjojose05@gmail.com>
2026-02-28 11:59:38 +0300

    Cors added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 80;
 
+app.use(cors());
 app.use(express.json());
 
 const customersRouter = require('./routes/customers');
